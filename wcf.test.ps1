ee8b714a6f02187f919160a0fa35b1ea7e9a9348
Justin Dearing <zippy1981@gmail.com>
2012-02-11 13:49:10 -0500

    Added a -UseFiddler switch.

--- wcf.test.ps1
+++ wcf.test.ps1
@@ -1,16 +1,26 @@
+param([switch] $UseFiddler)
+
 . .\wcf.ps1
 
-$wsdlImporter = Get-WsdlImporter 'http://localhost.fiddler:14232/EchoService.svc/mex'
-# Get-WsdlImporter 'http://localhost.fiddler:14232/EchoService.svc' -HttpGet
-# Get-WsdlImporter 'http://localhost.fiddler:14232/EchoService.svc?wsdl' -HttpGet 
+if ($UseFiddler) {
+  $hostname = 'localhost.fiddler'
+}
+else {
+  $hostname = 'localhost'
+}
+
+$wsdlImporter = Get-WsdlImporter "http://$($hostname):14232/EchoService.svc/mex"
+# Get-WsdlImporter "http://$($hostname):14232/EchoService.svc" -HttpGet
+# Get-WsdlImporter "http://$($hostname):14232/EchoService.svc?wsdl" -HttpGet 
 
 # $proxyType = Get-WcfProxyType $wsdlImporter
 
 # $endpoints = $wsdlImporter.ImportAllEndpoints();
 # $proxy = New-Object $proxyType($endpoints[0].Binding, $endpoints[0].Address);
-$proxy = Get-WcfProxy 'http://localhost.fiddler:14232/EchoService.svc/mex'
+#$proxy = Get-WcfProxy "http://$($hostname):14232/EchoService.svc/mex"
+$proxy = Get-WcfProxy $wsdlImporter
 $proxy.Echo("Justin Dearing");
 
-Get-Help Get-WsdlImporter
-Get-Help Get-WcfProxyType
-Get-Help Get-WcfProxy
+#Get-Help Get-WsdlImporter
+#Get-Help Get-WcfProxyType
+#Get-Help Get-WcfProxy
